42f57e49636500159869a505848e566262f5f256
Niclas <niclastimmdev@gmail.com>
2022-06-08 10:07:42 +0200

    Add logging

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
+import logging
 from fastapi import Depends, FastAPI
 from app.db.db import engine
 from app.db import models
 from app.api.routers import router
+
+# logging.config.fileConfig('logger.conf', disable_existing_loggers=False)
+# logger = logging.getLogger(__name__)
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -12,4 +16,5 @@ app.include_router(router)
 
 @app.get('/')
 async def front():
+    # logger.info("logging from the root logger")
     return "Lieber Arthur, du bist ein kleiner Pisser :)!"
