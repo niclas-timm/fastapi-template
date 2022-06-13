2d9b915b1a07901b5ba8e35daf2d9073a74607cd
Niclas <niclastimmdev@gmail.com>
2022-06-13 15:26:22 +0200

    Refactor DB connection settings.

--- app/core/db/db.py
+++ app/core/db/db.py
@@ -8,27 +8,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from pathlib import Path
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+from app.core import config
 
-
-def get_db_settings():
-    """Get database environment variables.
-
-    Returns:
-        _dict_: Environment variables related to DB connection.
-    """
-    return {
-        "DB_USERNAME": os.getenv('DB_USERNAME'),
-        "DB_PASSWORD": os.getenv('DB_PASSWORD'),
-        "DB_HOST": os.getenv('DB_HOST'),
-        "DB_PORT": os.getenv('DB_PORT'),
-        "DB_NAME": os.getenv('DB_NAME'),
-    }
-
-
-settings = get_db_settings()
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.get('DB_USERNAME')}:{settings.get('DB_PASSWORD')}@{settings.get('DB_HOST')}:{settings.get('DB_PORT')}/{settings.get('DB_NAME')}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{config.DB_USERNAME}:{config.DB_PASSWORD}@{config.DB_HOST}:{config.DB_PORT}/{config.DB_NAME}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
